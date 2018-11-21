25e6f13453728830614f500e3d825203fd53443b
user <denis.semikin123@gmail.com>
2018-11-21 14:53:53 +0200

    Add timeout for init postgresql db

--- python/django-rest/start.sh
+++ python/django-rest/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -e
+
+while ! (timeout 3 bash -c "</dev/tcp/${POSTGRES_HOST}/${POSTGRES_PORT}") &> /dev/null;
+do
+    echo waiting for PostgreSQL to start...;
+    sleep 3;
+done;
+
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
