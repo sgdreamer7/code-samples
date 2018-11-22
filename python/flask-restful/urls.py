ccee63ad12dfc0fe7be7b06b1e60b069d732a56e
user <denis.semikin123@gmail.com>
2018-11-22 12:31:06 +0200

    Remove reduntant code

--- python/flask-restful/urls.py
+++
@@ -1,8 +0,0 @@
-from app import api
-from resources.category import CategoryResource
-from resources.comment import CommentResource
-from resources.hello import Hello
-
-api.add_resource(Hello, '/hello')
-api.add_resource(CategoryResource, '/category')
-api.add_resource(CommentResource, '/comment')
